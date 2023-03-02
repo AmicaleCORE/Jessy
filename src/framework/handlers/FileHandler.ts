30ba4aa785e74c334f0fffc3a6f667cd7a2d8cad
loicmaes <production@maesloic.fr>
2023-03-02 03:54:56 +0100

    :tada: First handler : file handler

---
+++ src/framework/handlers/FileHandler.ts
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+module.exports = function handle(path: string, subDirectories?: boolean): string[] {
+    const list: string[] = []
+
+    fs.readdirSync(path).forEach((file: string) => {
+        if (subDirectories && fs.lstatSync(`${path}/${file}`).isDirectory()) handle(file, true).forEach((f: string) => list.push(`${file}/${f}`))
+        else if (file.endsWith(`.ts`)) list.push(`${file}`)
+    })
+
+    return list
+}
