878d32c85e8b703baf0777aaf32c544b43df86a0
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-03-21 00:47:07 +0100

    :rocket: Apply foreign keys

--- src/framework/functions/Models.ts
+++ src/framework/functions/Models.ts
@@ -1,3 +1,19 @@
-export function apply_foreign_keys() {
+import {models} from "~/framework/database/Orm";
 
+export function apply_foreign_keys() {
+    models.ticket.hasMany(models.assign, {
+        foreignKey: 'ticket_id',
+        primaryKey: true,
+        validate: {
+            isNumeric: true
+        }
+    })
+
+    models.ticket.hasMany(models.admin, {
+        foreignKey: 'ticket_id',
+        primaryKey: true,
+        validate: {
+            isNumeric: true
+        }
+    })
 }
