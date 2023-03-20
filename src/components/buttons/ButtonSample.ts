4cbc371c36f47466d6afb0b2db96c1ba7784aa26
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-03-20 23:00:58 +0100

    :tada: Brand new project

--- src/components/buttons/ButtonSample.ts
+++
@@ -1,7 +0,0 @@
-import {Button, ButtonOptions} from "~/framework/structures/Component";
-import Bot from "~/framework/structures/Bot";
-import {ButtonInteraction} from "discord.js";
-
-module.exports = new Button("button-sample", async (bot: Bot, interaction: ButtonInteraction, placeholder?: string) => {
-    await interaction.showModal(require('~/components/modals/ModalSample').build())
-}, new ButtonOptions("Click to see", "👀"))
